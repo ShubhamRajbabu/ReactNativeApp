f8b19357bd1adafc8040f04848d28916d412940b
ShubhamRajbabu <shubhamshroff0810@gmail.com>
2024-04-25 17:39:00 +0530

    Bookmark changes

--- app/(tabs)/bookmark.jsx
+++ app/(tabs)/bookmark.jsx
@@ -1,12 +1,15 @@
 import { View, Text } from 'react-native'
 import React from 'react'
+import { SafeAreaView } from 'react-native-safe-area-context'
 
 const Bookmark = () => {
   return (
-    <View>
-      <Text>Bookmark</Text>
-    </View>
+    <SafeAreaView className = 'bg-black h-full mt-5'>
+      <Text className='text-2xl text-white font-semibold'>Your saved videos</Text>
+      <Text className='text-base font-normal text-gray-300 mt-3'>Currently work is in progress</Text>
+    </SafeAreaView>
   )
 }
+
 
 export default Bookmark
